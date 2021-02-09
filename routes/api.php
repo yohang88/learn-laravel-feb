64fd2aae28aef4adb67cb4bb609af57ff444fead
Yoga Hanggara <yohang88@gmail.com>
2021-02-09 11:26:46 +0700

    initial CRUD Book

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/health', function () {
+    return response()->json(["message" => "Hello World"]);
+});
+
+Route::apiResource('books', BookController::class);
